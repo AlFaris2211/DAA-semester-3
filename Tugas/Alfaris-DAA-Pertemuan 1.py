822fb5aa4212d5650c15c46679fc7543f8a61f13
fish <143941771+AlFaris2211@users.noreply.github.com>
2024-09-07 11:39:09 +0700

    Add files via upload

---
+++ Tugas/Alfaris-DAA-Pertemuan 1.py
@@ -0,0 +1,12 @@
+# Langkah 1: Masukkan angka pertama
+angka_pertama = int(input("Masukkan angka pertama: "))
+
+# Langkah 2: Masukkan angka kedua
+angka_kedua = int(input("Masukkan angka kedua: "))
+
+# Langkah 3: Buat kondisi pertama
+if angka_pertama > angka_kedua:
+    print("Angka terbesar adalah:", angka_pertama)
+# Langkah 4: Buat kondisi kedua
+else:
+    print("Angka terbesar adalah:", angka_kedua)
